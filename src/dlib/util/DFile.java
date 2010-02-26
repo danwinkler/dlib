86309ec1917100d5f11b1669f1a3bcd3695a445c
Daniel Winkler <danthemanwinkler@gmail.com>
2010-02-26 14:18:39 -0600

    ImageWindow works now

--- src/dlib/util/DFile.java
+++ src/dlib/util/DFile.java
@@ -29,7 +29,6 @@ public class DFile
 		oos.close();
 	}
 	
-	/*
 	public String loadText( String filename ) throws FileNotFoundException
 	{
 		Scanner scanner = new Scanner( filename );
@@ -41,6 +40,7 @@ public class DFile
 		FileOutputStream fos = new FileOutputStream( filename );
 	}
 	
+	/**
 	public Image loadImage( String filename ) throws FileNotFoundException
 	{
 		FileInputStream fis = new FileInputStream( filename );
