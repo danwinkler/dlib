da31754a5fc41d5b26dd19f67fac96cf7201c7fe
Daniel Winkler <danthemanwinkler@gmail.com>
2010-04-16 18:00:38 -0500

    added jogl files

---
+++ src/dlib/graphics/renderer/GLRenderer.java
@@ -0,0 +1,221 @@
+package dlib.graphics.renderer;
+
+import java.awt.Image;
+import java.awt.event.KeyListener;
+
+import dlib.graphics.Renderer;
+
+public class GLRenderer implements Renderer 
+{
+	
+	@Override
+	public void addKeyListener(KeyListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void begin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beginShape(ShapeType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void box(float width, float height, float length) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawImage(Image img, float x, float y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ellipse(float x, float y, float width, float height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endShape() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fill(int c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fill(float r, float g, float b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fill(float r, float g, float b, float a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void frameRate(float r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void line(float x1, float y1, float x2, float y2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void line(float x1, float y1, float z1, float x2, float y2, float z2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void popMatrix() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pushMatrix() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rect(float x, float y, float width, float height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotate(float angle, float vx, float vy, float vz) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotate(float angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotateX(float angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotateY(float angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void rotateZ(float angle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scale(float x, float y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void scale(float x, float y, float z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void size(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stroke(int c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stroke(float r, float g, float b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stroke(float r, float g, float b, float a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void text(String text, float x, float y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void texture(Image img) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void translate(float x, float y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void translate(float x, float y, float z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vertex(float x, float y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vertex(float x, float y, float z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
