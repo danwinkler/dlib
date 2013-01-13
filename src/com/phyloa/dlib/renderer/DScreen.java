77025a39f41797e6f2b99ddd528dce10f5aa2338
Daniel Winkler <danthemanwinkler@gmail.com>
2013-01-12 23:54:33 -0600

    Modifications to DScreen to allow use with Slick

--- src/com/phyloa/dlib/renderer/DScreen.java
+++ src/com/phyloa/dlib/renderer/DScreen.java
@@ -1,6 +1,9 @@
 package com.phyloa.dlib.renderer;
 
-public interface DScreen<E>
+public interface DScreen<E, F>
 {	
-	public void update( E e );
+	public void update( E e, int delta );
+	public void render( E e, F f );
+	public void onActivate( E e, DScreenHandler<E, F> dsh );
+	public void onExit();
 }
--- src/com/phyloa/dlib/renderer/DScreenHandler.java
+++ src/com/phyloa/dlib/renderer/DScreenHandler.java
@@ -2,34 +2,41 @@ package com.phyloa.dlib.renderer;
 
 import java.util.HashMap;
 
-public class DScreenHandler<E>
+public class DScreenHandler<E, F>
 {
-	HashMap<String, DScreen<E>> screens = new HashMap<String, DScreen<E>>();
+	HashMap<String, DScreen<E, F>> screens = new HashMap<String, DScreen<E, F>>();
 	
-	DScreen<E> d = null;
+	DScreen<E, F> d = null;
 	
-	public void register( String s, DScreen<E> d )
+	public void register( String s, DScreen<E, F> d )
 	{
 		screens.put( s, d );
-		if( this.d == null )
-		{
-			this.d = d;
-		}
 	}
 	
-	public void activate( String s )
+	public void activate( String s, E e )
 	{
+		if( d != null )
+		{
+			d.onExit();
+		}
 		d = screens.get( s );
+		d.onActivate( e, this );
 	}
 	
-	public DScreen<E> get( String s )
+	public DScreen<E, F> get( String s )
 	{
 		return screens.get( s );
 	}
 	
-	public void update( E e )
+	public void update( E e, int delta )
 	{
 		if( d != null )
-			d.update( e );
+			d.update( e, delta );
+	}
+	
+	public void render( E e, F f )
+	{
+		if( d != null )
+			d.render( e, f );
 	}
 }
