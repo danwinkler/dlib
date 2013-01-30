601d195739670ddb6687dde200366596bcce13df
Daniel Winkler <danthemanwinkler@gmail.com>
2013-01-30 08:10:33 -0600

    added DScreenHandler

--- src/com/phyloa/dlib/renderer/DScreenHandler.java
+++ src/com/phyloa/dlib/renderer/DScreenHandler.java
@@ -8,6 +8,11 @@ public class DScreenHandler<E, F>
 	
 	DScreen<E, F> d = null;
 	
+	DScreenTransition<E, F> out;
+	DScreenTransition<E, F> in;
+	
+	DScreen<E, F> nextScreen;
+	
 	public void register( String s, DScreen<E, F> d )
 	{
 		screens.put( s, d );
@@ -28,6 +33,19 @@ public class DScreenHandler<E, F>
 		}
 	}
 	
+	public void activate( String s, E e, DScreenTransition<E, F> out, DScreenTransition<E, F> in )
+	{
+		this.out = out;
+		this.in = in;
+		
+		nextScreen = get( s );
+		
+		if( out != null )
+		{
+			out.init( e );
+		}
+	}
+	
 	public DScreen<E, F> get( String s )
 	{
 		return screens.get( s );
@@ -37,8 +55,48 @@ public class DScreenHandler<E, F>
 	{
 		synchronized( d )
 		{
-			if( d != null )
-				d.update( e, delta );
+			if( nextScreen != null )
+			{
+				if( out != null )
+				{
+					if( out.isFinished() )
+					{
+						if( d != null )
+						{
+							d.onExit();
+						}
+						d = nextScreen;
+						d.gc = e;
+						d.dsh = this;
+						d.onActivate( e, this );
+						nextScreen = null;
+						out = null;
+						if( in != null )
+						{
+							in.init( e );
+						}
+					}
+					else
+					{
+						out.update( e, delta );
+					}
+				}
+			}
+			else
+			{
+				if( in != null )
+				{
+					in.update( e, delta );
+					if( in.isFinished() )
+					{
+						in = null;
+					}
+				}
+				if( d != null )
+				{
+					d.update( e, delta );
+				}
+			}
 		}
 	}
 	
@@ -46,8 +104,34 @@ public class DScreenHandler<E, F>
 	{
 		synchronized( d )
 		{
+			if( nextScreen != null )
+			{
+				if( out != null )
+				{
+					out.renderPre( e, f );
+				}
+			}
+			else if( in != null )
+			{
+				in.renderPre( e, f );
+			}
+			
 			if( d != null )
+			{
 				d.render( e, f );
+			}
+			
+			if( nextScreen != null )
+			{
+				if( out != null )
+				{
+					out.renderPost( e, f );
+				}
+			}
+			else if( in != null )
+			{
+				in.renderPost( e, f );
+			}
 		}
 	}
 
