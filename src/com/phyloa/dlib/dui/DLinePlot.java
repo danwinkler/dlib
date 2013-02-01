56bb27d4c94eb2b27b437974adf7b5bdff113174
Daniel Winkler <danthemanwinkler@gmail.com>
2013-02-01 06:45:59 -0600

    Added labels and fixed divideby0 bug in DLinePlot

--- src/com/phyloa/dlib/dui/DLinePlot.java
+++ src/com/phyloa/dlib/dui/DLinePlot.java
@@ -3,14 +3,24 @@ package com.phyloa.dlib.dui;
 import java.awt.Color;
 import java.util.ArrayList;
 
+import javax.vecmath.Vector2f;
+
 import com.phyloa.dlib.renderer.Renderer2D;
 import com.phyloa.dlib.util.DMath;
 
 public class DLinePlot extends DUIElement
 {
+	ArrayList<String[]> labels = new ArrayList<String[]>();
 	ArrayList<Color> colors = new ArrayList<Color>();
 	ArrayList<float[]> values = new ArrayList<float[]>();
 	float maxValue = 0;
+	
+	String labelToShow;
+	float lx;
+	float ly;
+	Color labelColor;
+	
+	public float labelShowDist = 10;
 	
 	public DLinePlot( int width, int height )
 	{
@@ -36,13 +46,25 @@ public class DLinePlot extends DUIElement
 		{
 			g.color(  colors.get( i ) );
 			float[] vals = values.get( i );
+			if( vals.length <= 1 ) continue;
 			float xScale = width / (vals.length-1);
 			for( int j = 0; j < vals.length-1; j++ )
 			{
 				g.line( j*xScale, height - vals[j] * yScale, (j+1)*xScale, height - vals[j+1] * yScale );
 			}
 		}
+		
+		if( labelToShow != null )
+		{
+			g.color( labelColor );
+			g.drawOval( lx-5, ly-5, 10, 10 );
+			Vector2f strSize = g.getStringSize( labelToShow );
+			g.text( labelToShow, lx-strSize.x/2, ly - 10 - strSize.y );
+		}
+		
 		g.popMatrix();
+		
+		g.setLineWidth( 1 );
 	}
 
 	public void update( DUI ui )
@@ -54,6 +76,12 @@ public class DLinePlot extends DUIElement
 	{
 		colors.add( c );
 		values.add( arr );
+		String[] label = new String[arr.length];
+		for( int i = 0; i < arr.length; i++ )
+		{
+			label[i] = Float.toString( arr[i] );
+		}
+		labels.add( label );
 		float max = DMath.maxf( arr );
 		if( max > maxValue )
 			maxValue = max;
@@ -62,11 +90,18 @@ public class DLinePlot extends DUIElement
 	public void addLine( int[] arr, Color c )
 	{
 		float[] v = new float[arr.length];
+		String[] label = new String[arr.length];
 		for( int i = 0; i < v.length; i++ )
 		{
 			v[i] = arr[i];
+			label[i] = Integer.toString( arr[i] );
 		}
-		addLine( v, c );
+		colors.add( c );
+		values.add( v );
+		labels.add( label );
+		float max = DMath.maxf( v );
+		if( max > maxValue )
+			maxValue = max;
 	}
 	
 	public void keyPressed( DKeyEvent dke )
@@ -110,24 +145,51 @@ public class DLinePlot extends DUIElement
 		
 	}
 
-	@Override
 	public void mouseMoved( DMouseEvent e )
 	{
-		// TODO Auto-generated method stub
-		
+		calcMouseHover( e.x-x, e.y-y );
 	}
 
-	@Override
 	public void mouseDragged( DMouseEvent e )
 	{
-		// TODO Auto-generated method stub
-		
+	
 	}
 
-	@Override
 	public void mouseWheel( DMouseEvent dme )
 	{
-		// TODO Auto-generated method stub
 		
+	}
+	
+	public void calcMouseHover( int mx, int my )
+	{
+		float dist = Float.MAX_VALUE;
+		float yScale = height / maxValue;
+		for( int i = 0; i < colors.size(); i++ )
+		{
+			float[] vals = values.get( i );
+			float xScale = width / (vals.length-1);
+			for( int j = 0; j < vals.length; j++ )
+			{
+				float tx = j*xScale;
+				float ty = height - vals[j] * yScale;
+				
+				float dx = tx-mx;
+				float dy = ty-my;
+				float td = dx*dx + dy*dy;
+				if( td < dist )
+				{
+					dist = td;
+					lx = tx;
+					ly = ty;
+					labelToShow = labels.get( i )[j];
+					labelColor = colors.get( i );
+				}
+			}
+		}
+		
+		if( dist > labelShowDist*labelShowDist )
+		{
+			labelToShow = null;
+		}
 	}
 }
